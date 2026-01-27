b50715abce9d99e287dbdfedd8e61daaeca1a962
achi3v3 <amir-tutaev@mail.ru>
2026-01-27 22:48:00 +0500

    feat(back): add comments

--- backend/main-service/internal/logger/logger.go
+++ backend/main-service/internal/logger/logger.go
@@ -1,3 +1,4 @@
+// Package logger provides logging configuration
 package logger
 
 import (
@@ -6,13 +7,19 @@ import (
 	"time"
 )
 
+// NewLogger initializes application logger
 func NewLogger() {
 	file, err := os.OpenFile("/logs/app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Printf("Failed to open log file: %v", err)
 		return
 	}
-	defer file.Close()
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Printf("error with file close %v", err)
+			return
+		}
+	}()
 
 	logger := log.New(file, "APP: ", log.LstdFlags)
 
