398ce2845f5659760a572d0ec5b06ff6c8eae30e
achi3v3 <amir-tutaev@mail.ru>
2026-01-27 22:48:05 +0500

    feat(back): add comments

--- backend/main-service/internal/models/models.go
+++ backend/main-service/internal/models/models.go
@@ -1,5 +1,7 @@
+// Package models defines application data structures
 package models
 
+// Link represents shortened URL entity
 type Link struct {
 	OriginLink string `json:"origin_link"`
 	PseudoLink string `json:"pseudo_link"`
